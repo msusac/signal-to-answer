b7580826bc6a3abf20c02289364b57efa171b45a
DEV\mario.susac <mario.susac@king-ict.hr>
2022-06-04 13:25:34 +0200

    Added Scheduler job for deactivating Guest accounts that are inactive for more than 24 hours.

---
+++ SignalToAnswer/Jobs/User/DeactivateGuestJob.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.Logging;
+using Quartz;
+using SignalToAnswer.Attributes;
+using SignalToAnswer.Constants;
+using SignalToAnswer.Services;
+using System;
+using System.Threading.Tasks;
+
+namespace SignalToAnswer.Jobs
+{
+    [DisallowConcurrentExecution]
+    public class DeactivateGuestJob : IJob
+    {
+        private readonly ILogger<DeactivateGuestJob> _logger;
+        private readonly ConnectionService _connectionService;
+        private readonly GroupService _groupService;
+        private readonly UserService _userService;
+
+        public DeactivateGuestJob(ILogger<DeactivateGuestJob> logger, ConnectionService connectionService, GroupService groupService, UserService userService)
+        {
+            _logger = logger;
+            _connectionService = connectionService;
+            _groupService = groupService;
+            _userService = userService;
+        }
+
+        [Transactional]
+        public async Task Execute(IJobExecutionContext context)
+        {
+            var group = await _groupService.GetOneUnique(GroupType.OFFLINE);
+            var users = await _userService.GetAll(RoleType.GUEST, group.Id.Value);
+            var count = 0;
+
+            foreach (var user in users)
+            {
+                var connection = await _connectionService.GetOne(user.Id);
+
+                if (DateTime.Now.Subtract(connection.UpdatedAt).Hours > 1)
+                {
+                    await _connectionService.Deactivate(connection);
+                    await _userService.Deactivate(user);
+                    count++;
+                }
+            }
+
+            if (count > 0)
+            {
+                _logger.LogInformation("{count} Guest account deactivated.", count);
+            }
+        }
+    }
+}
