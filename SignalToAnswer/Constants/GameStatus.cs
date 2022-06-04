297fc2d48f09ead0b2e073917a9870fc02031677
DEV\mario.susac <mario.susac@king-ict.hr>
2022-06-04 13:13:40 +0200

    Added new values for GameStatus.cs.

--- SignalToAnswer/Constants/GameStatus.cs
+++ SignalToAnswer/Constants/GameStatus.cs
@@ -2,12 +2,14 @@
 {
     public static class GameStatus
     {
-        public static readonly int CREATED = 1;
-        public static readonly int PLAYERS_ADDED = 2;
-        public static readonly int QUESTIONS_ADDED = 3;
-        public static readonly int READY_FOR_GAME = 4;
-        public static readonly int IN_PROGRESS = 5;
-        public static readonly int GAME_END = 6;
-        public static readonly int PLAYER_DISCONNECTED = 7;
+        public static readonly int WAITING_FOR_PLAYERS_TO_CONNECT = 1;
+        public static readonly int WAITING_FOR_PLAYERS_TO_ACCEPT_INVITE = 2;
+        public static readonly int PLAYERS_CONNECTED = 3;
+        public static readonly int QUESTIONS_ADDED = 4;
+        public static readonly int READY_FOR_GAME = 5;
+        public static readonly int IN_PROGRESS = 6;
+        public static readonly int GAME_END = 7;
+        public static readonly int PLAYER_DISCONNECTED = 8;
+        public static readonly int CANCELLED = 9;
     }
 }
