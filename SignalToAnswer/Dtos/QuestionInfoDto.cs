19ca130cad7ddb03dd62a61adb49a0be678cf391
DEV\mario.susac <mario.susac@king-ict.hr>
2022-06-22 13:22:52 +0200

    Added dto classes.

---
+++ SignalToAnswer/Dtos/QuestionInfoDto.cs
@@ -0,0 +1,19 @@
+﻿namespace SignalToAnswer.Dtos.GameHub
+{
+    public class QuestionInfoDto
+    {
+        public int Row { get; set; }
+
+        public int TotalRows { get; set; }
+
+        public string Description { get; set; }
+
+        public int Category { get; set; }
+
+        public int Difficulty { get; set; }
+
+        public int ScoreMultiplier { get; set; }
+
+        public string CorrectAnswer { get; set; }
+    }
+}
---
+++ SignalToAnswer/Dtos/ResultInfoDto.cs
@@ -0,0 +1,33 @@
+﻿namespace SignalToAnswer.Dtos.GameHub
+{
+    public class ResultInfoDto
+    {
+        public string Player { get; set; }
+
+        public int Score { get; set; }
+
+        public int? CurrentAnswerStatus { get; set; }
+
+        public int? CurrentAnswerScore { get; set; }
+
+        public ResultInfoDto(string player, int score)
+        {
+            Player = player;
+            Score = score;
+            CurrentAnswerStatus = 0;
+            CurrentAnswerScore = 0;
+        }
+
+        public ResultInfoDto(string player, int score, int currentAnswerStatus) : this(player, score)
+        {
+            CurrentAnswerStatus = currentAnswerStatus;
+            CurrentAnswerScore = 0;
+        }
+
+        public ResultInfoDto(string player, int score, int currentAnswerStatus, int currentAnswerScore) : this(player, score)
+        {
+            CurrentAnswerStatus = currentAnswerStatus;
+            CurrentAnswerScore = currentAnswerScore;
+        }
+    }
+}
