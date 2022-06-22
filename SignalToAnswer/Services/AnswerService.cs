3d13f2cde862e898ad41e100b5643a62256d0236
DEV\mario.susac <mario.susac@king-ict.hr>
2022-06-22 13:21:45 +0200

    Added service classes.

---
+++ SignalToAnswer/Services/AnswerService.cs
@@ -0,0 +1,34 @@
+﻿using SignalToAnswer.Attributes;
+using SignalToAnswer.Entities;
+using SignalToAnswer.Repositories;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SignalToAnswer.Services
+{
+    public class AnswerService
+    {
+        private readonly AnswerRepository _answerRepository;
+
+        public AnswerService(AnswerRepository answerRepository)
+        {
+            _answerRepository = answerRepository;
+        }
+
+        public async Task<List<Answer>> GetAllIncorrect(int gameId, int matchId, int playerId, int questionId) 
+        {
+            return await _answerRepository.FindAllByGameIdAndMatchIdAndPlayerIdNotAndQuestionIdAndIsCorrectAnswerFalse(gameId, matchId, playerId, questionId);
+        }
+
+        public async Task<Answer> GetOneQuietly(int gameId, int matchId, int playerId, int questionId)
+        {
+            return await _answerRepository.FindOneByGameIdAndMatchIdAndPlayerIdAndQuestionId(gameId, matchId, playerId, questionId);
+        }
+
+        [Transactional]
+        public async Task<Answer> Save(Answer answer)
+        {
+            return await _answerRepository.Save(answer);
+        }
+    }
+}
---
+++ SignalToAnswer/Services/MatchService.cs
@@ -0,0 +1,46 @@
+﻿using SignalToAnswer.Attributes;
+using SignalToAnswer.Entities;
+using SignalToAnswer.Repositories;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SignalToAnswer.Services
+{
+    public class MatchService
+    {
+        private readonly MatchRepository _matchRepository;
+
+        public MatchService(MatchRepository matchRepository)
+        {
+            _matchRepository = matchRepository;
+        }
+
+        [Transactional]
+        public async Task<Match> CreateMatch(Game game)
+        {
+            var match = new Match
+            {
+                Game = game,
+                GameId = game.Id.Value
+            };
+
+            return await _matchRepository.Save(match);
+        }
+
+        public async Task<List<Match>> GetAllFinished(int gameId)
+        {
+            return await _matchRepository.FindAllByGameIdAndOngoingFalse(gameId);
+        }
+
+        public async Task<Match> GetOneLatest(int gameId)
+        {
+            return await _matchRepository.FindOneByGameIdAndIsOngoingTrueOrderedByCreatedAtDesc(gameId);
+        }
+
+        public async Task<Match> Deactivate(Match match)
+        {
+            match.Active = false;
+            return await _matchRepository.Save(match);
+        }
+    }
+}
